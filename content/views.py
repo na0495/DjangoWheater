e627bb0e2942c69ff44affcb472f323a72879308
na4095 <saadmrabet008@gmail.com>
2021-06-29 22:13:54 +0100

    update views

--- content/views.py
+++ content/views.py
@@ -1,8 +1,25 @@
 from django.shortcuts import render
 import requests
+from .models import City
 
 def index(request):
     url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=92e355e9126abf9ca9ac2a472f2348d6'
-    city = 'Las Vegas'
-    city_weather = requests.get(url.format(city)).json() #request the API data and convert the JSON to Python data types
+    cities = City.objects.all()
+    weather_data = []
+
+    for city in cities:
+
+        city_weather = requests.get(url.format(city)).json() #request the API data and convert the JSON to Python data types
+
+        weather = {
+            'city' : city,
+            'temperature' : city_weather['main']['temp'],
+            'description' : city_weather['weather'][0]['description'],
+            'icon' : city_weather['weather'][0]['icon']
+        }
+
+        weather_data.append(weather) #add the data for the current city into our list
+
+    context = {'weather_data' : weather_data}
+
     return render(request, 'content/index.html') #returns the index.html template
